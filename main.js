670d224788e3e728917c06983fa91c70ba5d3cd0
dabudidabuday <94mir.ms@gmail.com>
2019-10-29 12:01:10 +0200

    value in progress

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+//  *** MODAL OPEN & CLOSE ***
+
+let    open = document.querySelector('.modal__open'),
+  container = document.querySelector('.modal__container'),
+      modal = document.querySelector('.modal');
+
+function openModal() {
+    container.style.display='block';
+    modal.style.display='block';
+};
+
+function closeModal() {
+    container.style.display='none';
+    modal.style.display="none";
+};
+
+open.addEventListener('click', function() {
+    openModal();
+});
+
+container.addEventListener('click', function() {
+    closeModal();
+});
+
+// *** SEND MODAL VALUE (Email, Password) ***
+
 let email = document.querySelector('.modal__email'),
      pass = document.querySelector('.modal__pass'),
    submit = document.querySelector('.modal__submit-field');
+
+function collectValue() {
+    let emailValue = String(email.value),
+         passValue = String(pass.value);
+}
+
+submit.addEventListener('click', function() {
+    console.log(collectValue()),
+    closeModal();
+})
